ad72388faeafedb738e1fa8be5342252b12b5925
juunnn <junaedi.fahmi15081@student.unsika.ac.id>
2019-10-01 13:32:19 +0700

    commit ke dua

---
+++ voxforge/makemfcc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This code is used to prepare data from making lang data until mfcc
+
+. ./path.sh || exit 1;
+. ./cmd || exit 1;
+
+dialects="British"
+
+nspk_test=20
+
+lm_order=2
+
+pos_dep_phones=true
+
+selected=${DATA_ROOT}/selected
+
+. utils/parse_options.sh || exit 1;
+
+[[ $# -ge 1 ]] && { echo "Unexpected Args"; exit 1;}
+
+local/voxforge_select.sh --dialect $dialects \
+	${DATA_ROOT}/extracted ${selected} || exit 1;
+
+local/voxforge_map_anonymous.sh ${selected} || exit 1;
+
+local/voxforge_data_prep.sh --nspk_test ${nspk_test} ${selected} || exit 1;
+
+local/voxforge_prepare_lm.sh --order ${lm_order} || exit 1;
+
+lcoal/voxforge_prepare_dict.sh || exit 1;
+
+utils/prepare_lang.sh --position-dependent-phones $pos_dep_phones \
+	data/local/dict '!SIL' data/local/lang data/lang || exit 1
+
+local/voxforge_format_data.sh || exit 1;
+
+mfccdir=${DATA_ROOT}/mfcc
+
+for x in train test; do
+	steps/make_mfcc.sh exp/make_mfcc/$x $mfccdir || exit 1;
+	steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+done
+
