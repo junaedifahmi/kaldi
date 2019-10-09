261a8b8962fb5889e22569674838686cb4426314
juunnn <junaedi.fahmi15081@student.unsika.ac.id>
2019-10-09 13:33:26 +0700

    add cmd and path

---
+++ voxforge/myrecipes/chain_tri.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir_exp=./exp/chain
+
+dir_lang=
+dir_train=
+dir_test=
+
+
--- voxforge/myrecipes/exp_ddelta.sh
+++ voxforge/myrecipes/exp_ddelta.sh
@@ -12,8 +12,8 @@ if [ ! -d exp/tri_delta/align ]; then
 		exp/tri_delta/align;
 fi
 
-for gauss in 5000 6000 7000 8000 9000 10000; do
-	for leaves in 7000 8000 9000 10000 11000 12000; do
+for gauss in {5000..10000..1000}; do
+	for leaves in {6000..1200..1000}; do
 		dir_exp=${dir_base}/${x}_${y}
 		train_start=$(date +"%T")
 		./steps/train_deltas.sh --cmd "$train_cmd" \
@@ -22,7 +22,7 @@ for gauss in 5000 6000 7000 8000 9000 10000; do
 			${dir_exp}/model;
 		train_end=$(date +"%T")
 		elapsed=$(( $(date -d "$train_end" "+%s") - $(date -d "$train_start" "+%s") ))
-		echo elapsed >> ${dir_exp}/training_time.txt
+		echo $elapsed >> ${dir_exp}/training_time.txt
 
 		./utils/mkgraph.sh \
 			data/lang_test ${dir_exp}/model \
@@ -33,7 +33,7 @@ for gauss in 5000 6000 7000 8000 9000 10000; do
 			${dir_exp}/graph data/test \
 			${dir_exp}/model/decode;
 		train_end=$(date +"%T")
-		echo elapsed >> ${dir_exp}/decode_time.txt
+		echo $elapsed >> ${dir_exp}/decode_time.txt
 
 		for x in ${dir_exp}/*/decode*; do
 			[ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh >> ${dir_base}/best_wer.txt;
